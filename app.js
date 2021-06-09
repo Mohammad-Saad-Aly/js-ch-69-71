7e2a6614abdcd9744b33e1f940bcc82a9ab9b79a
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-06-09 20:12:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// var  stud= {
+//     name : "saad",
+//     age : 21,
+//     fathname : "Muzaffar",
+//     roll : "CSC-088",
+//     arr : ["yasir","atif"],
+//     rrr : {
+//         fname : "usama",
+//         lname : "rafeh"
+//     },
+//     merge : function(){
+//         return this.name+" "+this.fathname
+//     }
+// }
+// document.write(stud.merge())
+// stud.disount = 7.8
+// stud.hh = ["jaja","jkhjkahs"]
+// var ss = {};
+// ss.name = "bgasbg"
+// // console.log(ss)
+
+// delete stud.roll
+// delete stud.rrr.fname
+
+// ggg = "namejjj" in stud
+// document.write(ggg)
+
+
+// console.log(stud)
+// console.log(stud)
+
+// var arr = ["saad","ali"]
+// document.write(arr)
+
+// var obj = new Object();
+// obj.name = "saad"
+// obj.fname = "ali"
+// console.log(obj)
+
+
+
